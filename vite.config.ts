d3f97eb1efedade4bdaab5fa3a65938700d89a2b
Joanne Lin <y3933y3933@gmail.com>
2025-06-17 15:38:57 +0800

    chore: add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -25,4 +25,13 @@ export default defineConfig({
       "@": resolve(__dirname, "./src"),
     },
   },
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+  },
 });
